61ec5c4bf2ffd42cac24ba6e3939849744c93b7a
Grade <aborecevskij94@gmail.com>
2024-03-15 15:13:58 +0200

    Implemented GCD algorithm

---
+++ src/euclid/tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+from .utils import gcd
+
+
+class TestGCD(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(gcd(10, 25), 5)
+
+    def test_2(self):
+        self.assertEqual(gcd(14, 15), 1)
+
+    def test_3(self):
+        self.assertEqual(gcd(3, 9), 3)
